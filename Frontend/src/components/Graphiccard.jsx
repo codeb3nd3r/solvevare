3dfe624acbfac8c8e86e812638f11ef46ba6eba3
SUFYAN AKBAR (  Code Bender ) <128650086+codeb3nd3r@users.noreply.github.com>
2023-12-23 20:52:12 +0500

    Update Graphiccard.jsx(2)

--- Frontend/src/components/Graphiccard.jsx
+++ Frontend/src/components/Graphiccard.jsx
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react'
 
 const Graphiccard = ({ img }) => {
   const [card, setCard] = useState(null)
-  const clickme=()=alert("Feature Cooming Soon")>;
+  const clickme=()=>alert("Feature Cooming Soon")>;
   return (
     <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
 
