354da323b5742c4e200a7e35b80b591b70df9e5d
SUFYAN AKBAR (  Code Bender ) <128650086+codeb3nd3r@users.noreply.github.com>
2024-05-13 21:43:17 +0500

    Update Hero.jsx
    changed description

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -16,9 +16,7 @@ const Hero = () => {
           Solutions through software.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+          We provide solutions with software
         </p>
       </div>
 
